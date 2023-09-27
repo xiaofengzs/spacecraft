aeb69163a0af58cb993924eda5437d9a9039d35e
Xiaofeng Zhi <zhixiaofengzs@gmail.com>
2023-09-27 16:35:55 +0800

    Get space craft from cluster.

---
+++ api/v1/spacecraft_types.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// SpacecraftSpec defines the desired state of Spacecraft
+type SpacecraftSpec struct {
+	Image string `json:"image"`
+	Port  int32  `json:"port"`
+
+	// +kubebuilder:validation:Maximum=5
+	// +kubebuilder:validation:Minimum=1
+	Replicas int32 `json:"replicas"`
+
+	// +kubebuilder:validation:Optional
+	EnableService bool `json:"enableService,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	ServiceConfig ServiceConfig `json:"serviceConfig"`
+}
+
+// ServiceConfig defines the desired state of ServiceConfig
+type ServiceConfig struct {
+
+	// +kubebuilder:validation:Optional
+	Type       string `json:"type,omitempty"`
+	Port       int32  `json:"port"`
+	TargetPort int32  `json:"targetPort"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Maximum=32767
+	// +kubebuilder:validation:Minimum=30000
+	NodePort int32 `json:"nodePort,omitempty"`
+}
+
+
+// SpacecraftStatus defines the observed state of Spacecraft
+type SpacecraftStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Spacecraft is the Schema for the spacecrafts API
+type Spacecraft struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SpacecraftSpec   `json:"spec,omitempty"`
+	Status SpacecraftStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SpacecraftList contains a list of Spacecraft
+type SpacecraftList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Spacecraft `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Spacecraft{}, &SpacecraftList{})
+}
---
+++ internal/controller/spacecraft_controller.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	xiaofengv1 "cloud/spacecraft/api/v1"
+)
+
+// SpacecraftReconciler reconciles a Spacecraft object
+type SpacecraftReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=xiaofeng.cloud,resources=spacecrafts,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=xiaofeng.cloud,resources=spacecrafts/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=xiaofeng.cloud,resources=spacecrafts/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Spacecraft object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.0/pkg/reconcile
+func (r *SpacecraftReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	log.Log.Info("receive request", "key", req.NamespacedName)
+
+	spaceCraft := &xiaofengv1.Spacecraft{}
+
+	if err := r.Client.Get(context.TODO(), req.NamespacedName, spaceCraft); err != nil {
+		if errors.IsNotFound(err) {
+			log.Log.Info("This space craft spec has beed delete", "space craft", req.NamespacedName)
+			return ctrl.Result{}, nil
+		}
+		log.Log.Error(err, "Get spaceCraft error")
+		return ctrl.Result{}, err
+	}
+
+	
+
+	log.Log.Info("Get space craft from cluster", "spaceCraft", spaceCraft)
+
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *SpacecraftReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&xiaofengv1.Spacecraft{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
+}
